e2e0ad431f01911b2bedfe2b64eb423be66455be
NoobforAl <58856931+NoobforAl@users.noreply.github.com>
2023-03-12 22:59:00 +0330

    fix b question to better answer
    Signed-off-by: NoobforAl <58856931+NoobforAl@users.noreply.github.com>

--- B Question/main.go
+++ B Question/main.go
@@ -2,39 +2,30 @@ package main
 
 import "fmt"
 
-type find struct {
-	index, value int
-}
-
-func foundSingle(arr *[]int) *find {
+func foundSingle(arr *[]int) int {
 	keys := make(map[int]int)
-	read := make(map[int]int)
 
 	for i, v := range *arr {
 		if _, ok := keys[v]; ok {
 			delete(keys, v)
 		} else {
-			if _, ok := read[v]; !ok {
-				keys[v] = i
-			} else {
-				read[v] = 0
-			}
+			keys[v] = i
 		}
 	}
 
-	for v, i := range keys {
-		return &find{index: i, value: v}
+	for v := range keys {
+		return v
 	}
 
-	return nil
+	return -1
 }
 
 func main() {
 	arr := []int{2, 2, 5, 6, 5}
-	res := foundSingle(&arr)
+	ans := foundSingle(&arr)
 
-	if res != nil {
-		fmt.Printf("Index : %d\nValue : %d", res.index, res.value)
+	if ans != -1 {
+		fmt.Printf("Value : %d", ans)
 	} else {
 		fmt.Println("NotFound!")
 	}
